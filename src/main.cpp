146f682d6b2613ed25b1026c1fcedf45e495e7eb
Phan Quoc Huy <huypq020607@gmail.com>
2026-09-15 21:28:14 +0700

    Improve game loop timing

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include "../include/SnakeGame.h"
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <windows.h>
+using namespace std;
+int main(){
+	//tao seed random
+	srand(static_cast<unsigned int>(time(0)));
+	//an con tro
+	hideCursor();
+	const int gameSpeed = 60;
+	SnakeGame game;
+	while(!game.IsGameOver()){
+		game.Draw();
+		game.Input();
+		game.Logic();
+		Sleep(gameSpeed); //dieu chinh toc do game (ms)
+	}
+	gotoXY(0, 23);
+	cout << "============================" << endl;
+   	cout << "        GAME OVER!          " << endl;
+    	cout << "============================" << endl;
+    	cout << "Nhan phim bat ky de thoat..." << endl;
+    	system("pause"); // Dung man hinh de ban xem diem so truoc khi dong
+	return 0;
+}
